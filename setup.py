ffc380d560a536c38d387c20ebfb53faef2e8de4
Victor Vasiliev <vasilvv@mit.edu>
2013-09-07 21:41:55 -0400

    Initial check-in

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='discuss-ng',
+      version='1.0',
+      description='User interface front-end to the discuss network forum system',
+      author='Victor Vasiliev',
+      scripts=['meeting', 'ndsc']
+      )
